a018464aafb508ecb3d97abc9a53b5a0032b95a5
Stéphane Busiere <7332724-BusiereStephane@users.noreply.gitlab.com>
2024-02-19 16:12:33 +0100

    implementing firsts acf blocks

--- faust-app/wp-templates/front-page.js
+++ faust-app/wp-templates/front-page.js
@@ -1,74 +1,125 @@
-import { useQuery, gql } from '@apollo/client';
+import { gql } from '@apollo/client';
 import * as MENUS from '../constants/menus';
 import { BlogInfoFragment } from '../fragments/GeneralSettings';
+import { flatListToHierarchical } from '@faustwp/core';
+import { WordPressBlocksViewer } from '@faustwp/blocks';
+import blocks from '../wp-blocks';
+import components from '../wp-blocks';
+
 import {
   Header,
   Footer,
   Main,
   Container,
+  ContentWrapper,
+  EntryHeader,
   NavigationMenu,
-  Hero,
+  FeaturedImage,
   SEO,
 } from '../components';
 
-export default function Component() {
-  const { data } = useQuery(Component.query, {
-    variables: Component.variables(),
-  });
+export default function Component(props,loading, data) {
+  // Loading state for previews
+  if (props.loading) {
+    return <>Loading...</>;
+  }
 
   const { title: siteTitle, description: siteDescription } =
-    data?.generalSettings;
-  const primaryMenu = data?.headerMenuItems?.nodes ?? [];
-  const footerMenu = data?.footerMenuItems?.nodes ?? [];
+    props?.data?.generalSettings;
+  const primaryMenu = props?.data?.headerMenuItems?.nodes ?? [];
+  const footerMenu = props?.data?.footerMenuItems?.nodes ?? [];
+  const { title, content, featuredImage, editorBlocks } = props?.data?.page ?? { title: '' };
+  const blockList = flatListToHierarchical(editorBlocks, { childrenKey: 'innerBlocks' });
 
   return (
     <>
-      <SEO title={siteTitle} description={siteDescription} />
+      <SEO
+        title={siteTitle}
+        description={siteDescription}
+        imageUrl={featuredImage?.node?.sourceUrl}
+      />
       <Header
         title={siteTitle}
         description={siteDescription}
         menuItems={primaryMenu}
       />
       <Main>
-        <Container>
-          <Hero title={'Front Page'} />
-          <div className="text-center">
-            <p>This page is utilizing the "front-page" WordPress template.</p>
-            <code>wp-templates/front-page.js</code>
-          </div>
-        </Container>
+        <>
+          <Container>
+          <WordPressBlocksViewer blocks={blockList} />
+          </Container>
+        </>
       </Main>
       <Footer title={siteTitle} menuItems={footerMenu} />
     </>
   );
 }
 
+Component.variables = ({ databaseId }, ctx) => {
+  return {
+    databaseId,
+    headerLocation: MENUS.PRIMARY_LOCATION,
+    footerLocation: MENUS.FOOTER_LOCATION,
+    asPreview: ctx?.asPreview,
+  };
+};
+
 Component.query = gql`
+${blocks.CoreParagraph.fragments.entry}
+  ${blocks.CoreColumns.fragments.entry}
+  ${blocks.CoreColumn.fragments.entry}
+  ${blocks.CoreCode.fragments.entry}
+  ${blocks.CoreButtons.fragments.entry}
+  ${blocks.CoreButton.fragments.entry}
+  ${blocks.CoreQuote.fragments.entry}
+  ${blocks.CoreImage.fragments.entry}
+  ${blocks.CoreSeparator.fragments.entry}
+  ${blocks.CoreList.fragments.entry}
+  ${blocks.CoreHeading.fragments.entry}
   ${BlogInfoFragment}
   ${NavigationMenu.fragments.entry}
+  ${FeaturedImage.fragments.entry}
   query GetPageData(
+    $databaseId: ID!
     $headerLocation: MenuLocationEnum
     $footerLocation: MenuLocationEnum
+    $asPreview: Boolean = false
   ) {
+    page(id: $databaseId, idType: DATABASE_ID, asPreview: $asPreview) {
+      title
+      content
+      ...FeaturedImageFragment
+      editorBlocks {
+        name
+        __typename
+        renderedHtml
+        id: clientId
+        parentId: parentClientId
+        ...${blocks.CoreParagraph.fragments.key}
+        ...${blocks.CoreColumns.fragments.key}
+        ...${blocks.CoreColumn.fragments.key}
+        ...${blocks.CoreCode.fragments.key}
+        ...${blocks.CoreButtons.fragments.key}
+        ...${blocks.CoreButton.fragments.key}
+        ...${blocks.CoreQuote.fragments.key}
+        ...${blocks.CoreImage.fragments.key}
+        ...${blocks.CoreSeparator.fragments.key}
+        ...${blocks.CoreList.fragments.key}
+        ...${blocks.CoreHeading.fragments.key}
+      }
+    }
     generalSettings {
       ...BlogInfoFragment
-    }
-    headerMenuItems: menuItems(where: { location: $headerLocation }) {
-      nodes {
-        ...NavigationMenuItemFragment
-      }
     }
     footerMenuItems: menuItems(where: { location: $footerLocation }) {
       nodes {
         ...NavigationMenuItemFragment
       }
     }
+    headerMenuItems: menuItems(where: { location: $headerLocation }) {
+      nodes {
+        ...NavigationMenuItemFragment
+      }
+    }
   }
 `;
-
-Component.variables = () => {
-  return {
-    headerLocation: MENUS.PRIMARY_LOCATION,
-    footerLocation: MENUS.FOOTER_LOCATION,
-  };
-};
